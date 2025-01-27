699b53acb86e0267c2c1915d7370b4ac4295d860
Chu <69946366+chumutt@users.noreply.github.com>
2025-01-27 13:15:38 -0600

    Update and rename tester to clarify; rename deploy to deploy.sh

---
+++ clarify; rename deploy to deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+# Run this and open http://localhost:1313 in your browser
+# and you should be able to see the site live as you work.
+
+hugo server --buildDrafts --disableFastRender
